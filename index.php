028b177653ea411987c7f7082b291361accca7ef
anishav473 <you@example.com>
2025-08-03 21:46:23 +0530

    ok

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center><marquee behavior="scroll" direction="center"><font color='blue'>Welcome to my PHP page!</font></marquee>
+    <?php
+    echo "<font color='blue'><h1>Welcome to my PHP</h1></font>";
+    echo "<br>";
+    echo "<font color='green'>This is my php certification</font>";
+    echo "<br>";
+    echo "<strong>Welcome to my PHP page!</strong>";
+    ?>
+    </center>
+</body>
+</html>
