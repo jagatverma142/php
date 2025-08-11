318a1cd03c509087a879bbf4f85362e6e4a968bb
anishav473 <you@example.com>
2025-08-11 18:56:44 +0530

    ok

---
+++ php_practise/variables_rules.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // PHP code goes here
+    echo"Hello World";
+    ?>
+    <?php
+    $name="John Doe";
+    echo "<h1 style='color:blue;'>my name is $name</h1>";
+    ?>
+    <H1 style="color: orange;">
+        my brother name is <?php echo $name; ?>
+    </H1>
+    <h2 style="color:blueviolet">
+        my son name is <?php echo $name; ?></h2>
+</body>
+</html>
